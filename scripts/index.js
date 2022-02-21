dc54baf1a683f8dec3f40307d54e78c148e92204
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-21 13:24:33 +0100

    added fecth api to load new advice on every click

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
 const idNumber = document.querySelector('.advice--id--number');
 const quoteText = document.querySelector('.advice--quote--text');
 const diceButton = document.querySelector('.advice--dice');
+const url = "https://api.adviceslip.com/advice";
+
+function getAdvices() { 
+    fetch(url)
+    .then((response) => {
+        // handle the response
+        return response.json();
+    })
+    .then((data) => {
+        let advices = data;
+        idNumber.textContent = advices.slip.id; // add new id to idNumber
+        quoteText.textContent = advices.slip.advice; // add new advice to diceButton
+    })
+    .catch(function (error) {
+        // handle the error
+        console.log(error);
+    });
+}
+
+diceButton.addEventListener('click', getAdvices); //  new quote on button click
+window.onload = getAdvices; // new advice on page load
